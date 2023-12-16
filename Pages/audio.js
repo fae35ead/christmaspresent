f84a5a9103485c5ea1f0eb8d16c49d97a38848c6
fae35ead <1227918165@qq.com>
2023-12-16 17:12:04 +0800

    创建新index.html

---
+++ Pages/audio.js
@@ -0,0 +1,11 @@
+﻿// script.js
+
+function playAudio() {
+    var audio = document.getElementById("myAudio");
+    audio.play();
+}
+
+function pauseAudio() {
+    var audio = document.getElementById("myAudio");
+    audio.pause();
+}
